0df4390bdfdd79fca99d99bb2c5985c5defe09ab
Kyoz <banminkyoz@gmail.com>
2018-07-09 23:45:51 +0700

    Update

---
+++ app/pji.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+'use strict';
+
+// Const fs = require('fs');
+
+// function initTemplate(data) {
+//   switch(data.type) {
+//     case 'Node':
+//     break;
+//     case 'Node CLI':
+//     break;
+//     default:
+//   }
+// }
